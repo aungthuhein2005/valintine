a986d34b59cd1d6066c4f065eda2939c210b8b80
aungthuhein2005 <132641868+aungthuhein2005@users.noreply.github.com>
2024-02-11 19:14:37 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function checkUser() {
   console.log("hey");
   console.log(await checkId(user_id));
   console.log(!user_id && !(await checkId(user_id)));
-  if (user_id && (await checkId(user_id))) {
+  if (user_id && !(await checkId(user_id))) {
     window.location.href = "login.html";
   }
 }
